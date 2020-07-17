3ae8645b268d9a4d5da55ed3034b52f727b3649d
falearq <falearq@gmail.com>
2020-07-17 17:46:52 -0500

    solo falta la paleta

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,7 +7,7 @@ const About = ()=>{
         <Layout>
         <h1>Acerca</h1>
         <p>TJ3K es un proyecto multimedia que se concentra en una infografía animada, un sitio web y una postal sonora. 
-            Está centrado en el análisis de dos películas japonesas de bajo presupuesto  que son referentes del cine de ciencia ficción y del subgénero del cyberpunk:  デスパウダー<i className='italics'>Death Powder</i>  de 1986 y 鉄男 <i className='italics'>Tetsuo The Iron Man</i> de 1989.  <Link to='/contact'>Contacto</Link></p>
+            Está centrado en el análisis de dos películas japonesas de bajo presupuesto  que son referentes del cine de ciencia ficción y del subgénero del cyberpunk:  デスパウダー<i className='italics'>Death Powder</i>  de 1986 dirigida por 泉谷 しげる Shigeru Izumiya; y 鉄男 <i className='italics'>Tetsuo The Iron Man</i> de 1989 dirigida por 塚本 晋也 Shinya Tsukamoto.  <Link to='/contact'>Contacto</Link></p>
         </Layout>
     )
 }
